07f3a98d9e9acaadea6cd8fdd1594a18d4eb031e
bangunsepto <111333542+bangunsepto@users.noreply.github.com>
2022-08-23 16:19:29 +0700

    project 3 akhir
    project ETL

--- app.py
+++ app.py
@@ -1,21 +1,75 @@
 #!/usr/bin/python3
+
+from multiprocessing.connection import Connection
 import os
 import json
-import psycopg2
+import sqlparse
+
+import pandas as pd
+import numpy as np
+
+import koneksipostgres
+import koneksi_warehouse
 
 if __name__ == '__main__':
-  #  print("Hello")
-  path = os.getcwd()
-  with open(path+'/'+'config.json') as file:
-    conf = json.load(file)['postgresql']
-  try:
-    conn = psycopg2.connect(host=conf['host'], 
-                            database=conf['db'], 
-                            user=conf['user'], 
-                            password=conf['pwd'], 
-                            port=conf['port'] # saya menggunakan port 2022 sebagai pembeda karena koneksi DB ke wsl nya
-                            )
-    print(f"[INFO] Sukses konek ke PostgreSQL ....")
-  except:
-    print(f"[INFO] Gagal konek ke PostgreSQL ....")
-  #print(conf)
+    print(f"[INFO] Service ETL Memulai .....")
+    conn_dwh, engine_dwh  = koneksi_warehouse.conn()
+    cursor_dwh = conn_dwh.cursor()
+
+    conf = koneksipostgres.config('postgresql')
+    conn, engine = koneksipostgres.psql_conn(conf)
+    cursor = conn.cursor()
+
+    path_query = os.getcwd()+'/query/'
+    query = sqlparse.format(
+        open(
+            path_query+'query.sql','r'
+            ).read(), strip_comments=True).strip()
+
+    querydimuser = sqlparse.format(
+        open(
+            path_query+'query_dim_users.sql','r'
+            ).read(), strip_comments=True).strip()
+
+    queryfactorders = sqlparse.format(
+        open(
+            path_query+'query_fact_orders.sql','r'
+            ).read(), strip_comments=True).strip()
+
+    querydwhdimuser = sqlparse.format(
+        open(
+            path_query+'dwh_dim_users.sql','r'
+            ).read(), strip_comments=True).strip()
+
+    querydwhfactorders = sqlparse.format(
+        open(
+            path_query+'dwh_fact_orders.sql','r'
+            ).read(), strip_comments=True).strip()
+
+    query_dwh = sqlparse.format(
+        open(
+            path_query+'dwh_design.sql','r'
+            ).read(), strip_comments=True).strip()
+    try:
+        print(f"[INFO] Service ETL Sedang Berjalan .....")
+        df = pd.read_sql(query, engine)
+        dfusers = pd.read_sql(querydimuser, engine)
+        dffact = pd.read_sql(queryfactorders, engine)
+        
+        cursor_dwh.execute(query_dwh)
+        cursor_dwh.execute(querydwhdimuser)
+        cursor_dwh.execute(querydwhfactorders)
+        conn_dwh.commit()
+
+        dfusers.to_sql('dim_users', engine_dwh, if_exists='append', index=False)
+        print(f"[INFO] Service ETL dim users sukses .....")
+        df.to_sql('dim_orders', engine_dwh, if_exists='append', index=False)
+        print(f"[INFO] Service ETL dim orders sukses .....")
+        dffact.to_sql('fact_orders', engine_dwh, if_exists='append', index=False)
+        print(f"[INFO] Service ETL fact orders sukses .....")
+    except:
+        print(f"[INFO] Service ETL is Gagal .....")
+
+    
+
+    
